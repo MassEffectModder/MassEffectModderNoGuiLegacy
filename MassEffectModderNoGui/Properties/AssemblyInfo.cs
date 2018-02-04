4995a4c5efddb5f012a699e045ccc4a55e941274
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2018-02-04 23:16:04 +0100

    bump version

--- MassEffectModderNoGui/Properties/AssemblyInfo.cs
+++ MassEffectModderNoGui/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a283fe02-fd28-474d-a214-0d8c8c41d062")]
-[assembly: AssemblyVersion("58")]
-[assembly: AssemblyFileVersion("58")]
+[assembly: AssemblyVersion("59")]
+[assembly: AssemblyFileVersion("59")]
