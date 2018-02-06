f290cf12c6d94a476162a440906c7c8816fa4712
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2018-02-06 21:14:51 +0100

    remove detection some dlc mods

--- MassEffectModderNoGui/Properties/AssemblyInfo.cs
+++ MassEffectModderNoGui/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a283fe02-fd28-474d-a214-0d8c8c41d062")]
-[assembly: AssemblyVersion("60")]
-[assembly: AssemblyFileVersion("60")]
+[assembly: AssemblyVersion("61")]
+[assembly: AssemblyFileVersion("61")]
