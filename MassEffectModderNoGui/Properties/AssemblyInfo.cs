064a1bfb8a6189e100b5315506a5640ad0e19069
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2018-02-10 21:51:04 +0100

    load sfar into memory first then unpack

--- MassEffectModderNoGui/Properties/AssemblyInfo.cs
+++ MassEffectModderNoGui/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a283fe02-fd28-474d-a214-0d8c8c41d062")]
-[assembly: AssemblyVersion("65")]
-[assembly: AssemblyFileVersion("65")]
+[assembly: AssemblyVersion("66")]
+[assembly: AssemblyFileVersion("66")]
