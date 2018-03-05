6f4c19a0693e49d329fd8e084aa00c4d5b717c0b
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2018-03-05 08:02:47 +0100

    fixed cmd line argument

--- MassEffectModderNoGui/Program.cs
+++ MassEffectModderNoGui/Program.cs
@@ -536,8 +536,11 @@ namespace MassEffectModder
                 bool guiInstaller = false;
                 for (int l = 0; l < args.Length; l++)
                 {
-                    if (args[l].ToLowerInvariant() == "-alot-mode")
+                    if (args[l].ToLowerInvariant() == "-alot-mode" &&
+                        args[l].ToLowerInvariant() == "-gui-installer")
+                    {
                         guiInstaller = true;
+                    }
                 }
                 if (!CmdLineTools.InstallMods(gameId, input, ipc, repack, guiInstaller))
                     goto fail;
