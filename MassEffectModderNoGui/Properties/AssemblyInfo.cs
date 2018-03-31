1eb13fa3bde9cd86a3759c821bc5dbf50c4a2106
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2018-03-31 12:49:06 +0200

    remove empty mips check

--- MassEffectModderNoGui/Properties/AssemblyInfo.cs
+++ MassEffectModderNoGui/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a283fe02-fd28-474d-a214-0d8c8c41d062")]
-[assembly: AssemblyVersion("104")]
-[assembly: AssemblyFileVersion("104")]
+[assembly: AssemblyVersion("105")]
+[assembly: AssemblyFileVersion("105")]
